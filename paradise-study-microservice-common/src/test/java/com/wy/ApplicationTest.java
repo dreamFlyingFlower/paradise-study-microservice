0de88b932f3f327014fcfada1a1a646f6f0ae05e
ParadiseWY <582822832@qq.com>
2020-12-03 16:55:35 +0800

    study something

---
+++ paradise-study-microservice-common/src/test/java/com/wy/ApplicationTest.java
@@ -0,0 +1,20 @@
+package com.wy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * 
+ * 
+ * @author ParadiseWY
+ * @date 2020-12-03 16:21:34
+ * @git {@link https://github.com/mygodness100}
+ */
+@SpringBootTest
+public class ApplicationTest {
+
+	@Test
+	public void test() {
+
+	}
+}
