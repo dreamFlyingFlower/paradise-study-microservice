e89a29fbb43a7371adf0810890699bed3e5b1999
ParadiseWY <582822832@qq.com>
2020-12-05 10:39:25 +0800

    study something

--- paradise-study-microservice-bus/src/main/java/com/wy/Application.java
+++ paradise-study-microservice-bus/src/main/java/com/wy/Application.java
@@ -9,34 +9,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
- * 统一配置管理客户端
+ * 客户端配置自动刷新,需要使用cloud-bus和mq或kafka,仍然需要添加{@link RefreshScope}
  * 
- * @apiNote 因为config客户端从git或其他地方拉取配置是需要时间的,而一般拉取的都是appliction-xxx.yml,
- *          如果在application.yml中配置git服务器,那么就会形成冲突,需要将拉取配置的地址写在bootstrap.yml中,
- *          因为bootstrap.yml的读取在application.yml之前,一般该文件需要手动新建<br>
- *          不管是本地配置或jar启动参数中有同名属性,都使用远程的配置中的属性;远程配置没有的才使用本地配置
+ * @apiNote 添加依赖actuator,bus-amqp,安装rabbitmq,详见paradise-study-microservice/notes/MQ.md
  * 
- * @apiNote 手动配置刷新,config服务端不需要修改,在客户端需要刷新的类上添加{@link RefreshScope}注解,
- *          同时还需要添加actuator的依赖,在配置文件中开启refresh访问权限:<br>
- *          management.endpoints.web.exposure.include: health,info,refresh<br>
- *          post请求调用客户端ip:port/actuator/refresh等待刷新即可
+ * @apiNote 仍然可以手动刷新,关闭actuator的拦截,发送post请求到客户端ip:port/actuator/bus-refresh
+ *          自动刷新:需要在git配置的项目中添加webhooks,从项目的settings中进入->Webhooks->add webhook:
+ *          payload url:添加客户端需要发送post请求的地址,如客户端ip:port/actuator/bus-refresh,<br>
+ *          content-type:发送post请求的类型<br>
+ *          secret:若访问有密码,可添加密码,没有密码可不填<br>
+ *          选择发送的信息:可根据情况选择,只发送push事件,所有事件,自定义不发送事件,最后点击添加即可
  * 
- * @apiNote 自动刷新见paradise-study-microservice-bus
+ * @apiNote 自动刷新可以下载git专门的webhook在本地安装,可以确保安全,也可以在本地安装了git之后使用
  * 
  * @author ParadiseWY
  * @date 2020-12-04 09:29:28
  * @git {@link https://github.com/mygodness100}
  */
 @RequestMapping
-@SpringBootApplication
 @RefreshScope
+@SpringBootApplication
 public class Application {
 
 	@Value("${server.port}")
 	String name;
-
-	@Value("${server.context-path}")
-	String contextPath;
 
 	@Value("${server.servlet.encoding.charset}")
 	String charset;
@@ -55,7 +51,6 @@ public class Application {
 	@GetMapping("getConfig")
 	public String getConfig() {
 		System.out.println(name);
-		System.out.println(contextPath);
 		System.out.println(charset);
 		System.out.println(configName);
 		return null;
