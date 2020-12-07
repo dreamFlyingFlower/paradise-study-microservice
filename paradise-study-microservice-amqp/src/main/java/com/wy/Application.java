3d3c242e11e5b4491d510e2fa7c9b0dc7027d94e
ParadiseWY <582822832@qq.com>
2020-12-07 20:43:44 +0800

    study something

--- paradise-study-microservice-amqp/src/main/java/com/wy/Application.java
+++ paradise-study-microservice-amqp/src/main/java/com/wy/Application.java
@@ -18,15 +18,20 @@ import org.springframework.jms.annotation.EnableJms;
  * Amqp是JMS的实现,Spring中主要配置类为{@link JmsAutoConfiguration},{@link RabbitAutoConfiguration},<br>
  * 若需要使用JMS或Amqp,需要开启{@link EnableJms}或{@link EnableRabbit}
  * 
- * 支持的消息类型:TextMessage,MapMessage,BytesMessage,StreamMessage,ObjectMessage,<br>
- * 实际上都是{@link Message}的子接口,而且消息传递时实际上都是序列化成byte[]传输
+ * JMS支持的消息类型:TextMessage,MapMessage,BytesMessage,StreamMessage,ObjectMessage,<br>
+ * 实际上都是{@link Message}的子接口,而Amqp的消息传递时实际上都是序列化成byte[]传输,可以转换为复杂的其他类型
  * 
  * 消息队列的多种形式,不同实现有不同形式,但以下两种是都有的:<br>
  * 队列(queue):点对点消息通信,消息被放在队列中,可以有多个队列同时获取消息,但是消息只能被消费一次,消费完就移除
  * 主题(topic):发布(publish),订阅(subscribu)消息通信,消息被发送到主题,多个订阅者监听该主题,消息被消费多次
  * 
  * RabbitMQ提供了5种消息模式:除了direct,其他模型都和主题类似,只是在路由上有所不同<br>
- * direct: 点对点模型 fanout: topic: headers: system:
+ * direct:点对点模型,exchange的路由键必须和binding上指定的路由键完全匹配<br>
+ * fanout:广播模式,只要是发送到binding的消息,都会发送到绑定的队列中<br>
+ * topic:主题模式,根据路由键的匹配规则接收消息,类似于正则,其中#表示0个及以上字符,*表示单个字符<br
+ * headers:匹配AMQP消息的header而不是路由键,和direct交换器一样,但是性能差,几乎不用
+ * 
+ * AmqpAdmin:Amqp管理组件
  * 
  * @author ParadiseWY
  * @date 2020-12-07 16:56:43
