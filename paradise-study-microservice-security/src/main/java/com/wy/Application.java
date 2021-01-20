f470b976d3cc09f59a305ed37c5f4469731cc6b9
ParadiseWY <582822832@qq.com>
2021-01-20 17:38:42 +0800

    study something

--- paradise-study-microservice-security/src/main/java/com/wy/Application.java
+++ paradise-study-microservice-security/src/main/java/com/wy/Application.java
@@ -2,20 +2,34 @@ package com.wy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.access.expression.SecurityExpressionRoot;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 /**
- * springsecurity默认的登录请求是/login,而且必须是post请求
- * https://blog.csdn.net/abcwanglinyong/article/details/80981389
- * https://blog.csdn.net/qq_29580525/article/details/79317969
- * https://blog.csdn.net/XlxfyzsFdblj/article/details/82083443 实现动态权限控制
- * {@link https://www.cnblogs.com/softidea/p/7068149.html}
+ * springsecurity默认的登录请求是/login,而且必须是post请求<br>
+ * https://blog.csdn.net/abcwanglinyong/article/details/80981389<br>
+ * https://blog.csdn.net/qq_29580525/article/details/79317969<br>
+ * https://blog.csdn.net/XlxfyzsFdblj/article/details/82083443 实现动态权限控制<br>
+ * {@link https://www.cnblogs.com/softidea/p/7068149.html}<br>
  * 
- * 实现session的redis共享,可实现集群,使用EnableRedisHttpSession注解, 配置redis地址,可简单实现
+ * {@link EnableGlobalMethodSecurity}:开启Security安全管理<br>
+ * {@link UserDetailsService#loadUserByUsername}:该方法定义登录时的具体行为
+ * {@link UserDetails}:具体的用户实现类需要实现该接口,权限方法等需要在该类中添加
+ * {@link EnableGlobalMethodSecurity#prePostEnabled()}:是否开启{@link PreAuthorize}注解功能
+ * {@link PreAuthorize}:该注解用来管理角色,权限等,值为SpringEL表达式,解析规则{@link SecurityExpressionRoot}
+ * ->角色比较:在放入角色时需要在角色之前添加ROLE_,但是使用的时候是直接使用角色即可
+ * -->如放入时为ROLE_ADMIN,使用的时候写ADMIN即可,hasAnyRole('ADMIN')或hasRole('ADMIN')
+ * ->权限比较:在放入权限时直接放入即可,使用的时候直接写一样的即可
  * 
- * @apiNote EnableRedisHttpSession:通过redis开启session的集群共享功能,或者通过配置文件的sorttype配置
- * @author paradiseWy
- * @date 2019年1月31日 下午12:09:33
+ * {@link EnableRedisHttpSession}:通过redis开启session的集群共享功能,或者通过配置文件的sorttype配置
+ * 
+ * @author 飞花梦影
+ * @date 2019-01-31 00:09:33
+ * @git {@link https://github.com/mygodness100}
  */
 @EnableRedisHttpSession
 @SpringBootApplication
--- paradise-study-microservice-security/src/main/java/com/wy/entity/User.java
+++ paradise-study-microservice-security/src/main/java/com/wy/entity/User.java
@@ -12,6 +12,7 @@ import lombok.Setter;
 
 /**
  * 配合security登录,并且使用数据库的数据,必须实现UserDetails接口
+ * 
  * @author paradiseWy
  */
 @Getter
@@ -21,42 +22,62 @@ public class User implements UserDetails {
 	private static final long serialVersionUID = 1L;
 
 	private Integer userId;
+
 	private String username;
+
 	private String password;
+
 	private String realname;
+
 	private Integer departId;
+
 	private String idcard;
+
 	private Date birthday;
+
 	private Integer age;
+
 	private Character sex;
+
 	private String address;
+
 	private String email;
+
 	private String tel;
+
 	private String salary;
+
 	private Integer state;
+
 	private String userIcon;
+
 	private Date createtime;
+
 	private Date updatetime;
+
 	private String role = "ROLE_USER";
 
 	private boolean accountNonExpired;// 帐号失效,超时
+
 	private boolean accountNonLocked;// 帐号是否锁定
+
 	private boolean credentialsNonExpired;
+
 	private boolean enabled;
 
 	@Override
 	public String toString() {
-		return "User [userId=" + userId + ", username=" + username + ", password=" + password
-				+ ", realname=" + realname + ", departId=" + departId + ", idcard=" + idcard
-				+ ", birthday=" + birthday + ", age=" + age + ", sex=" + sex + ", address="
-				+ address + ", email=" + email + ", tel=" + tel + ", salary=" + salary + ", state="
-				+ state + ", userIcon=" + userIcon + ", createtime=" + createtime + ", updatetime="
-				+ updatetime + ", role=" + role + ", accountNonExpired=" + accountNonExpired
-				+ ", accountNonLocked=" + accountNonLocked + ", credentialsNonExpired="
-				+ credentialsNonExpired + ", enabled=" + enabled + "]";
+		return "User [userId=" + userId + ", username=" + username + ", password=" + password + ", realname=" + realname
+				+ ", departId=" + departId + ", idcard=" + idcard + ", birthday=" + birthday + ", age=" + age + ", sex="
+				+ sex + ", address=" + address + ", email=" + email + ", tel=" + tel + ", salary=" + salary + ", state="
+				+ state + ", userIcon=" + userIcon + ", createtime=" + createtime + ", updatetime=" + updatetime
+				+ ", role=" + role + ", accountNonExpired=" + accountNonExpired + ", accountNonLocked="
+				+ accountNonLocked + ", credentialsNonExpired=" + credentialsNonExpired + ", enabled=" + enabled + "]";
 	}
 
-	// 这是权限
+	/**
+	 * 设置角色权限,可以设置多个,用逗号隔开.角色前需要加上ROLE_,权限可以不加
+	 */
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
 		return AuthorityUtils.commaSeparatedStringToAuthorityList(role);
