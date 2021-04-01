11f1a24b5a04eb469488e048c5530cc072f407ef
ParadiseWY <582822832@qq.com>
2021-04-01 17:30:14 +0800

    study something

--- paradise-study-microservice-amqp/src/main/java/com/wy/activemq/producer/Producer3.java
+++ paradise-study-microservice-amqp/src/main/java/com/wy/activemq/producer/Producer3.java
@@ -5,6 +5,7 @@ import java.io.Serializable;
 import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.Session;
+import javax.jms.TextMessage;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.core.JmsTemplate;
@@ -21,10 +22,40 @@ import org.springframework.stereotype.Service;
 public class Producer3 {
 
 	@Autowired
-	private JmsTemplate template;
+	private JmsTemplate jmsTemplate;
 
 	public void sendMsg(String msg) {
-		template.send(new MessageCreator() {
+
+		jmsTemplate.send(new MessageCreator() {
+
+			/**
+			 * 创建一个要发送的序列化消息对象,返回这个消息对象,template自动将消息发送到MOM容器中
+			 */
+			@Override
+			public Message createMessage(Session session) throws JMSException {
+				return session.createTextMessage("spring发送的消息");
+			}
+		});
+
+	}
+
+	public void sendMsg(String queue, String msg) {
+
+		jmsTemplate.send(queue, new MessageCreator() {
+
+			/**
+			 * 创建一个要发送的序列化消息对象,返回这个消息对象,template自动将消息发送到MOM容器中
+			 */
+			@Override
+			public Message createMessage(Session session) throws JMSException {
+				return session.createTextMessage("spring发送的消息");
+			}
+		});
+
+	}
+
+	public void sendMsg() {
+		jmsTemplate.send(new MessageCreator() {
 
 			/**
 			 * 创建一个要发送的序列化消息对象,返回这个消息对象,template自动将消息发送到MOM容器中
@@ -32,10 +63,17 @@ public class Producer3 {
 			@Override
 			public Message createMessage(Session session) throws JMSException {
 				Message message = session.createObjectMessage(new Serializable() {
+
 					private static final long serialVersionUID = 1L;
+
 				});
 				return message;
 			}
 		});
+	}
+
+	public void testQueueReceive() throws JMSException {
+		TextMessage message = (TextMessage) jmsTemplate.receive("queue");
+		System.out.println(message.getText());
 	}
 }
