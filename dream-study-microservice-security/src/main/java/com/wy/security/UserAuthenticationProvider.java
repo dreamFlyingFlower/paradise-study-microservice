e90c98c666fb976191b5a41245bd066e9de6b04b
飞花梦影 <582822832@qq.com>
2022-05-18 23:53:51 +0800

    study something

--- dream-study-microservice-security/src/main/java/com/wy/security/UserAuthenticationProvider.java
+++ dream-study-microservice-security/src/main/java/com/wy/security/UserAuthenticationProvider.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.dao.AbstractUserDetailsAuthenticationProvider;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
@@ -16,11 +17,14 @@ import com.wy.entity.User;
 import com.wy.service.UserService;
 
 /**
- * 自定义登录的验证方法,从数据库中取出用户数据封装后返回
+ * 自定义登录的验证方法.可实现{@link AuthenticationProvider}或继承{@link AbstractUserDetailsAuthenticationProvider}
  * 
- * @apiNote 若需要登录验证,则请求的url中必须携带验证参数verifyType,该参数的值必须和需要验证的参数key相同,
- *          如verifyType=image,则需要验证的值的key必须是image,如image=需要验证的值
- * @author paradiseWy
+ * 若需要登录验证,则请求的url中必须携带验证参数verifyType,该参数的值必须和需要验证的参数key相同,
+ * 如verifyType=image,则需要验证的值的key必须是image,如image=需要验证的值
+ *
+ * @author 飞花梦影
+ * @date 2022-05-18 23:49:28
+ * @git {@link https://gitee.com/dreamFlyingFlower}
  */
 @Configuration
 public class UserAuthenticationProvider implements AuthenticationProvider {
@@ -29,11 +33,12 @@ public class UserAuthenticationProvider implements AuthenticationProvider {
 	private UserService userService;
 
 	@Override
-	public Authentication authenticate(Authentication authentication)
-			throws AuthenticationException {
-		String username = authentication.getName();// 这个获取表单输入中返回的用户名,用户名必须唯一
-		String password = (String) authentication.getCredentials();// 这个是表单中输入的密码
-		// 这里调用我们的自己写的获取用户的方法来判断用户是否存在和密码是否正确
+	public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+		// 这个获取表单输入中返回的用户名,用户名必须唯一
+		String username = authentication.getName();
+		// 这个是表单中输入的密码
+		String password = (String) authentication.getCredentials();
+		// 这里调用自己写的获取用户的方法来判断用户是否存在和密码是否正确
 		User user = (User) userService.loadUserByUsername(username);
 		if (user == null) {
 			throw new BadCredentialsException("用户名不存在");
