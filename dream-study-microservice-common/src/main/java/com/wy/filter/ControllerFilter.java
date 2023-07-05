569a30003424d2a7c14d19d7fb58abe1f6129dd1
DreamFlyingFlower <yang_wan@bjdv.com>
2023-07-05 18:01:59 +0800

    study something

--- dream-study-microservice-common/src/main/java/com/wy/filter/ControllerFilter.java
+++ dream-study-microservice-common/src/main/java/com/wy/filter/ControllerFilter.java
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
-import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson2.JSON;
 
 import lombok.extern.slf4j.Slf4j;
 
